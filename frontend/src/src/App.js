ccbbffdfe445d7361a3781644e015ccf8646994a
sanderkk <sander.k.kilen@gmail.com>
2020-09-14 10:56:10 +0200

    Add Jenkins pipeline, docker for deployment and basic dev environment
    Add Jenkinsfile for running CD
    
    Update .gitignore for dotnet application
    
    Add react-app template application
    
    Add dotnet web template
    
    Add nginx config file for nginx hosting
    
    Add docker-compose file and docker
    
    Update Jenkinsfile, resolve naming issue
    
    Edit endpoint response as CD test
    
    Edit endpoint response as CD test
    
    Edit endpoint response for github webhook test

---
+++ frontend/src/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Customer driven project, group 6
+        </p>
+        <p>
+          Sintef Ocean
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
