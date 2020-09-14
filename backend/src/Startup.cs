bf4684856a748a24c2417dee84750c38670bda83
sanderkk <sander.k.kilen@gmail.com>
2020-09-14 13:30:41 +0200

    Edit endpoint response as CD test

--- backend/src/Startup.cs
+++ backend/src/Startup.cs
@@ -32,7 +32,7 @@ namespace src
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello guys!");
                 });
             });
         }
