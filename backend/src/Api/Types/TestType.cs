e3cce4ab5e8cd64f1506568fccaca8aa4a7e6647
Sander Kilen <sander.k.kilen@gmail.com>
2020-09-24 15:25:06 +0200

    Create graphql connection

---
+++ backend/src/Api/Types/TestType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace src.Api.Types
+{
+    public class TestType : ITestType
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
