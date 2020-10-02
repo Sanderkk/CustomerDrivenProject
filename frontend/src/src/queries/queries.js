026cc4a67eaefef26750ce40ad5ae66d791c35f3
ivarnm <36505347+ivarnm@users.noreply.github.com>
2020-10-02 14:26:41 +0200

    enh: reflect changes in api

--- frontend/src/src/queries/queries.js
+++ frontend/src/src/queries/queries.js
@@ -1,10 +1,11 @@
 import { gql } from "@apollo/client";
 
-export const GET_TABLE_AND_COLUMNS = gql`
-  query getTableAndColumns {
-    tableAndColumns {
-      key
-      value
+export const GET_SENSORS = gql`
+  query getSensors {
+    sensors {
+      sensorColumns
+      sensorId
+      sensorTabel
     }
   }
 `;
