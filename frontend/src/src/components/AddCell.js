55d6df4aca95e031b2ccf7f4a2dc67cf496b1e79
mastoro <mastoro@stud.ntnu.no>
2020-10-12 12:25:00 +0200

    create form
    update form
    
    implement stock chart
    
    update query
    
    update options realtime and modify query builder
    
    rebase dev
    
    rebase dev
    
    rebase dev
    
    modify query builder
    
    fix indenting
    
    improve code based on comments on PR

--- frontend/src/src/components/AddCell.js
+++ frontend/src/src/components/AddCell.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import LineGraph from "./LineGraph";
 import QueryBuilder from "./QueryBuilder";
@@ -11,51 +11,94 @@ import './componentStyles/AddCell.css'
 //This component presents a page to the user where he/she can add a new cell
 //or modify an existing cell. 
 function AddCell() {
+  const [options, setOptions] = useState({ title: 'Title', primaryAxis: '', secondaryAxis: 'Value'})
 
-    const handleDiscard = () => {
-        //TODO: Discard cell
+  const handleDiscard = () => {
+    //TODO: Discard cell
+  }
+
+  const handleAddCell = () => {
+    //Check if any of the input-fields have been changed. 
+    const isEmpty = Object.values(options).every(x => (x === ''));
+    console.log(isEmpty)
+    if(!isEmpty) {
+      //TODO: send options and input to backend. Input is generated in QueryBuilder and saved in Redux store,
+      //while userOptions is generated in AddCell.
     }
+  }
 
-    const handleAddCell = () => {
-        //TODO: Add cell
-    }
+  //Form handling of the user's changes of the options.
+  const handleTitleChange = e => {
+    e.preventDefault()
+    setOptions({...options, title: e.target.value})
+  }
 
-    return(
-        <div>
-            <Navbar />
-            <div className="outer_flex_container">
-                <div className="add_cell_buttons">
-                   
-                    <div className="discard_cell" >
-                        <GlobalButton primary={false} btnText="Discard" handleButtonClick={handleDiscard}>
-                            <BiX />
-                        </GlobalButton>
-                    </div>
-                    <div className="add_cell" >
-                        <GlobalButton primary={true} btnText="Add Cell" handleButtonClick={handleAddCell}>
-                            <BiCheck />
-                        </GlobalButton>
-                    </div>
-                </div>
+  const handlePrimaryChange = e => {
+    e.preventDefault()
+    setOptions({...options, primaryAxis: e.target.value})
+  }
 
-                <div className="flex_container">
-                    <div className="graph_container">
-                        <div className="graph">
-                            <LineGraph />
-                        </div>
-                        <div className="query_builder">
-                            <QueryBuilder />
-                        </div>
-                    </div>
-                
-                    <div className="options">
-                        <h2>Options</h2>
-                    </div>
-                </div>
+  const handleSecondaryChange = e => {
+    e.preventDefault()
+    setOptions({...options, secondaryAxis: e.target.value})
+  }
+
+  return(
+    <div>
+      <Navbar />
+      <div className="outer_flex_container">
+        <div className="add_cell_buttons">
             
-            </div>
+          <div className="discard_cell" >
+            <GlobalButton primary={false} btnText="Discard" handleButtonClick={handleDiscard}>
+              <BiX />
+            </GlobalButton>
+          </div>
+          <div className="add_cell" >
+            <GlobalButton primary={true} btnText="Add Cell" handleButtonClick={handleAddCell}>
+              <BiCheck />
+            </GlobalButton>
+          </div>
         </div>
-    )
+
+        <div className="flex_container">
+          <div className="graph_container">
+            <div className="graph">
+              <LineGraph userOptions={options} />
+            </div>
+            <div className="query_builder">
+              <QueryBuilder />
+            </div>
+          </div>
+      
+          <div className="options-wrapper">
+            <h2>Graph Options</h2>
+
+            <form className="options-form">
+
+              <label className="options_label" htmlFor="title">
+                Chart Title:
+              </label>
+              <input type="text" id="title" onChange={handleTitleChange} value={options.title} />
+              
+              <label className="options_label" htmlFor="primaryAxis">
+                Right Y-axis:
+              </label>
+              <input type="text" id="primaryAxis" onChange={handlePrimaryChange} value={options.primaryAxis}/>
+
+              <label className="options_label" htmlFor="secondaryAxis">
+                Left Y-axis:
+              </label>
+              <input type="text" id="secondaryAxis" onChange={handleSecondaryChange} value={options.secondaryAxis}/>
+          
+            </form>
+
+          </div>
+        </div>
+      
+      </div>
+    </div>
+  )
 
 }
 
--- frontend/src/src/components/LineGraph.js
+++ frontend/src/src/components/LineGraph.js
@@ -1,128 +1,131 @@
 import React, { useEffect, useState } from 'react';
-import Highcharts from 'highcharts';
+import Highcharts from 'highcharts/highstock';
 import HighchartsReact from 'highcharts-react-official';
 import './componentStyles/LineGraph.css'
-
 import { useSelector } from "react-redux";
 
-//This component creates the graphs. The component listens to changes in the redux store, and
-//whenever the response of queryData is changed, the graph is updated with these values. 
-const DisplayHighcharts = () => {
-    const dataSeries = useSelector(state => state.queryData.response)
-    const [graphdata, setGraphData] = useState([])
-    
-    //TODO: remove this function when the data structure is changed in backend. 
-    const zip = function(ar1, ar2, zipper) {
-        return zipper
-            ? ar1.map((value, index) => zipper(value, ar2[index]))
-            : ar1.map((value, index) => [value, ar2[index]])
-            ;
-    }
 
+const DisplayHighcharts = ({userOptions}) => {
+
+   /*
+    Component for creating the actual graph. The component listent to changes in redux store,
+    and updates the graph whenever the data is changed in store. 
+
+    :param Object(String) userOptions: object containing the title and y-axis names. 
+    e.g: {title: 'Title', primaryAxis: 'Value', secondaryAxis: 'Value'}
+  */
+
+  const dataSeries = useSelector(state => state.queryData.response)
+  const [graphdata, setGraphData] = useState([])
+
+  useEffect(() => { 
     //Update the series displayed in the graph when state is updated in the store.
-    useEffect(() => { 
-        if(dataSeries !== undefined) {
-            let numberData = dataSeries.timeSeries.numberData
-                let dataFromStore = numberData.map((sensorData) => ({
-                    name: sensorData.key,
-                    data: sensorData.value,
-                    yAxis: determineAxis(sensorData.value[10])
-                }))
-                //TODO: use this line instead of the ones underneath when the data structure is changed in backend.
-                //setGraphData(dataFromStore)
-                
-                //TODO: remove these lines when the data structure is changed in backend. 
-                const t = parseTimes()
-                const d = transformData(dataFromStore, t)
-                setGraphData(d)
-            }
-     }, [dataSeries]);
-
-
-     //TODO: remove this function when the data structure is changed in backend. 
-     const parseTimes = () => {
-        let hours = []
-        for(let i = 0; i < dataSeries.timeSeries.time.length; i++) {
-            let dateObject = new Date(dataSeries.timeSeries.time[i]).getTime()
-            hours.push(dateObject)
-        }
-        return hours
+    if(dataSeries !== undefined) {
+      let numberData = dataSeries.timeSeries.data
+      if(numberData !== undefined) {
+        let dataFromStore = numberData.map((sensorData) => ({
+          name: sensorData.name,
+          data: sensorData.data,
+          pointStart: sensorData.startTime / 10000,
+          pointInterval: sensorData.interval / 10000,
+          yAxis: determineAxis(sensorData.data[10])
+        }))
+        setGraphData(dataFromStore)
+      }
     }
+  }, [dataSeries]);
 
-
-    //Determine whether the line should be on the right or left y-axis.
-    let axis2 = 0;
-    const determineAxis = (value) => {
-        if(value > 50){
-            axis2 = 1;  
-        }
-        else {
-            axis2 = 0;
-        }
-        return axis2;
+  //Determine whether the line should be on the right or left y-axis.
+  let axis2 = 0;
+  const determineAxis = (value) => {
+    if(value > 50){
+      axis2 = 1;  
     }
+    else {
+      axis2 = 0;
+    }
+    return axis2;
+  }
 
+  //Customizing the graph
+  const options = {
+    chart: {
+      type: 'line',
+      zoomType: 'x',
+    },
     
-    //TODO: remove this function when the data structure is changed in backend. 
-    const transformData = (sensors, time) => {
-        var listOfObjects = [];
-        sensors.map(sensor => {
-            var singleObj = {};
-            singleObj['data'] = zip(time, sensor.data);
-            singleObj['name'] = sensor.name;
-            listOfObjects.push(singleObj);
-        });
-        return listOfObjects
-    }
+    title: {
+      text: userOptions.title
+    },
+    colors: ['#004299', '#94f700', '#c70076', '#f7e818'],
+    xAxis: [{ 
+      type: 'datetime',
+      title: {
+          text: 'Date'
+      },
+    }],
+    rangeSelector: {
+      buttons: [
+      {
+        type: 'hour',
+        count: 1,
+        text: '1h'
+      }, {
+        type: 'day',
+        count: 1,
+        text: '1d'
+      }, {
+        type: 'week',
+        count: 1,
+        text: '1w'
+      }, {
+        type: 'month',
+        count: 1,
+        text: '1m'
+      }, {
+        type: 'month',
+        count: 6,
+        text: '6m'
+      }, {
+        type: 'year',
+        count: 1,
+        text: '1y'
+      }, {
+        type: 'all',
+        text: 'All'
+      }]
+    },
 
-    //Customizing the graph
-    const options = {
-        chart: {
-            type: 'line',
-            zoomType: 'x',
-        },
-        title: {
-            text: "Title"
-        },
-        colors: ['#004299', '#94f700', '#c70076', '#f7e818'],
-        xAxis: [{ 
-            type: 'datetime',
-            title: {
-                text: 'Date'
-            },
-        }],
-        // Primary yAxis
-        yAxis: [{ 
-            labels: {
-                format: '{value}',
-            },
-            title: {
-                text: 'Primary y-axis',
+    // Primary yAxis
+    yAxis: [{ 
+      labels: {
+        format: '{value}',
+      },
+      title: {
+        text: userOptions.primaryAxis,
+      },
+    },
+    // Secondary yAxis
+    { 
+      title: {
+        text: userOptions.secondaryAxis,
+      },
+      labels: {
+        format: '{value}',
+      },
+      opposite: false,
+    }],
+      // Data from the query builder.
+      series: graphdata 
+  };
 
-            },
-            opposite: true
-        },
-        // Secondary yAxis
-         { 
-            title: {
-                text: 'Secondary y-axis',
-            },
-            labels: {
-                format: '{value}',
-            }
-        }
-    ],
-        // Data from the query builder.
-        series: graphdata 
-    };
-
-    return (
-    <div className="chart_wrapper">
-        <div>
-            <HighchartsReact highcharts={Highcharts} options={options} />
-        </div>
+  return (
+  <div className="chart_wrapper">
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={options} constructorType={"stockChart"} />
     </div>
-    );
+  </div>
+  );
 }
 
 export default DisplayHighcharts;
