e28b3855ac1866bdc1a5ff969b1ffa1d3f5a97ee
ivarnm <36505347+ivarnm@users.noreply.github.com>
2020-10-14 10:48:12 +0200

    feat: add modal for viewing metadata

--- frontend/src/src/queries/queries.js
+++ frontend/src/src/queries/queries.js
@@ -28,3 +28,47 @@ export const GET_TIME_SERIES = gql`
     }
   }
 `;
+
+export const GET_LAST_METADATA = gql`
+  query lastMetadata($sensorID: Int!) {
+    lastMetadata(sensorID: $sensorID) {
+      sensorID
+      metadataID
+      number
+      actualDisposal
+      altitude
+      cableLength
+      checkOnInspectionRound
+      company
+      coordinate
+      createdAt
+      department
+      identificator
+      inspectionRound
+      lending
+      lendingPrice
+      locationDescription
+      measureArea
+      modelNumber
+      name
+      nextService
+      outdatedFrom
+      ownerID
+      picture
+      plannedDisposal
+      purchaseDate
+      serialNumber
+      servicePartner
+      signal
+      tag1
+      tag2
+      tag3
+      timeless
+      tollerance
+      updatedAt
+      voltage
+      warrantyDate
+      website
+    }
+  }
+`;
