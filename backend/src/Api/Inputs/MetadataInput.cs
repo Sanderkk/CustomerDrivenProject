5e5d0b4b620f0f817548d7741750e62f874aebdc
Emanuele <emanueca@stud.ntnu.no>
2020-10-21 12:14:45 +0200

    Fixed not nullable field showed as nullable in graphQL auto documentation

--- backend/src/Api/Inputs/MetadataInput.cs
+++ backend/src/Api/Inputs/MetadataInput.cs
@@ -1,10 +1,3 @@
-using HotChocolate;
-using HotChocolate.Types;
-using src.Api.Types;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using src.Api.Models;
 
 namespace src.Api.Inputs
