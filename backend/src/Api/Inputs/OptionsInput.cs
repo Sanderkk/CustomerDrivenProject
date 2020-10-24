2f48940d5dfa472164351342bb005e9be96ec345
Sebastian Aas <sebastian.aas@outlook.com>
2020-10-24 10:15:06 +0200

    bug: fix dashboard mutations

---
+++ backend/src/Api/Inputs/OptionsInput.cs
@@ -0,0 +1,9 @@
+﻿namespace src.Api.Inputs
+{
+    public class OptionsInput
+    {
+       public string LYAxis { get; set; } 
+       public string RYAxis { get; set; }
+       public string Title { get; set; }
+    }
+}
